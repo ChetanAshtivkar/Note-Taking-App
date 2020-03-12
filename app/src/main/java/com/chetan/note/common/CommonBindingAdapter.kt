6d7b39c8099b75d54b507f3040e6d6dc63cdbb98
Chetan Ashtivkar <chetan.ashtivkar@gmail.com>
2020-03-12 22:05:33 +0530

    Updated the note layout to display the title, content and the date on the list

---
+++ app/src/main/java/com/chetan/note/common/CommonBindingAdapter.kt
@@ -0,0 +1,17 @@
+package com.chetan.note.common
+
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ * Created by Chetan on 12/03/20.
+ */
+val format = SimpleDateFormat("dd MMMM yyyy, h:mm a")
+
+@BindingAdapter("app:last_update")
+fun setDate(textView: TextView, date: Date) {
+    val text = "Last updated on ${format.format(date)}"
+    textView.text = text
+}
